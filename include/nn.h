6cf6eb30078de2a2508669e28a738aad8cf34f27
RayhaanT <tanweer.rayhaan@gmail.com>
2024-01-11 21:57:20 -0500

    Neural network class for collecting layers

---
+++ include/nn.h
@@ -0,0 +1,27 @@
+#ifndef NN_H
+#define NN_H
+
+#include "layer.h"
+#include "bce.h"
+#include <vector>
+
+class NeuralNetwork {
+private:
+    std::vector<std::shared_ptr<Layer>> layers;
+    BCECost bce;
+
+    MatrixBuffer Y;
+    MatrixBuffer dY;
+    float learningRate;
+
+public:
+    NeuralNetwork(float learningRate, Shape shape);
+
+    MatrixBuffer inference(MatrixBuffer m);
+    void backprop(MatrixBuffer predictions, MatrixBuffer target);
+
+    void addLayer(std::shared_ptr<Layer> layer);
+    std::vector<Layer*> getLayers() const;
+};
+
+#endif
---
+++ src/nn.cu
@@ -0,0 +1,37 @@
+#include "nn.h"
+
+NeuralNetwork::NeuralNetwork(float learningRate, Shape shape) :
+	learningRate(learningRate), Y(shape), dY(shape)
+{
+    dY.allocate();
+}
+
+void NeuralNetwork::addLayer(std::shared_ptr<Layer> layer) {
+	this->layers.push_back(layer);
+}
+
+MatrixBuffer NeuralNetwork::inference(MatrixBuffer X) {
+	MatrixBuffer Z = X;
+	for (auto layer : layers) {
+		Z = layer->forward(Z);
+	}
+	Y = Z;
+	return Y;
+}
+
+void NeuralNetwork::backprop(MatrixBuffer predictions, MatrixBuffer target) {
+	MatrixBuffer error = bce.dCost(predictions, target, dY);
+	for (auto it = this->layers.rbegin(); it != this->layers.rend(); it++) {
+		error = (*it)->backprop(error, learningRate);
+	}
+	cudaDeviceSynchronize();
+}
+
+std::vector<Layer*> NeuralNetwork::getLayers() const {
+    std::vector<Layer*> ret;
+    ret.reserve(layers.size());
+    for (auto &l : layers) {
+        ret.emplace_back(l.get());
+    }
+	return ret;
+}
